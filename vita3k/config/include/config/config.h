2cabe0bd3297594c9dfc2475a27fa131a0626e69
Philipp Richter <richterphilipp.pops@gmail.com>
2024-03-03 19:33:51 +0100

    app, config, ctrl: add config options for CemuHookUDP
    * 'cemuhookudp-address': expects an address in the form of '127.0.0.1:26760'
    * 'cemuhookudp-gyro-scale-divider': modifies the gyro sensitivity. The
      calculation is done like this:
      gyro.x/y/z * (1.0 / cemuhookudp-gyro-scale-divider)
      So the higher the value the lower the sentivity and the lower the
      value the higher the sensitivity. The default is '39'.
      A value of 1 maps gyro input 1:1.

--- vita3k/config/include/config/config.h
+++ vita3k/config/include/config/config.h
@@ -159,7 +159,9 @@ enum ScreenshotFormat {
     code(int, "http-timeout-sleep-ms", 100, http_timeout_sleep_ms)                                      \
     code(int, "http-read-end-attempts", 10, http_read_end_attempts)                                     \
     code(int, "http-read-end-sleep-ms", 250, http_read_end_sleep_ms)                                    \
-    code(bool, "tracy-primitive-impl", false, tracy_primitive_impl)
+    code(bool, "tracy-primitive-impl", false, tracy_primitive_impl)                                     \
+    code(std::string, "cemuhookudp-address", "127.0.0.1:26760", cemuhookudp_address)                    \
+    code(float, "cemuhookudp-gyro-scale-divider", 39.0f, cemuhookudp_gyro_scale_divider)
 
 // Vector members produced in the config file
 // Order is code(option_type, option_name, default_value)
